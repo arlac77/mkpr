1652a7d7c46dc9ece5ff5b40b2a56ca9a4293dc6
Markus Felten <Markus.Felten@gmx.de>
2018-10-31 16:50:08 +0100

    chore(rollup): copy from template chore(git): update .gitignore from template

--- tests/rollup.config.js
+++ tests/rollup.config.js
@@ -1,42 +1,23 @@
-import multiEntry from "rollup-plugin-multi-entry";
-import resolve from "rollup-plugin-node-resolve";
-import commonjs from "rollup-plugin-commonjs";
-import istanbul from "rollup-plugin-istanbul";
-import base from "../rollup.config.js";
+import multiEntry from 'rollup-plugin-multi-entry';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import istanbul from 'rollup-plugin-istanbul';
 
-export default [
-  ...base,
-  {
-    input: "tests/**/*-test.js",
-    output: {
-      file: "build/bundle-test.js",
-      format: "cjs",
-      sourcemap: true,
-      interop: false
-    },
-    external: [
-      "ava",
-      "path",
-      "os",
-      "util",
-      "url",
-      "net",
-      "tty",
-      "https",
-      "fs",
-      "stream",
-      "events",
-      "assert",
-      "os",
-      "child_process"
-    ],
-    plugins: [
-      multiEntry(),
-      resolve(),
-      commonjs(),
-      istanbul({
-        exclude: ["tests/**/*-test.js", "node_modules/**/*"]
-      })
-    ]
-  }
-];
+export default {
+  input: 'tests/**/*-test.js',
+  output: {
+    file: 'build/bundle-test.js',
+    format: 'cjs',
+    sourcemap: true,
+    interop: false
+  },
+  external: ['ava'],
+  plugins: [
+    multiEntry(),
+    resolve(),
+    commonjs(),
+    istanbul({
+      exclude: ['tests/**/*-test.js', 'node_modules/**/*']
+    })
+  ]
+};
