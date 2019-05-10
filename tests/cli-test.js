3278cf3b44552a3f2ab5dbb39bc11880f8596dbf
Markus Felten <Markus.Felten@gmx.de>
2019-05-10 21:36:27 +0200

    test: aslo cleanup by deleting pr

--- tests/cli-test.js
+++ tests/cli-test.js
@@ -1,6 +1,7 @@
 import test from "ava";
 import execa from "execa";
 import { join } from "path";
+import { GithubProvider } from "github-repository-provider";
 
 test("cli", async t => {
   const p = await execa(join(__dirname, "..", "bin", "mkpr"), [
@@ -9,6 +10,18 @@ test("cli", async t => {
     "sed s/8.11/8.12/",
     "arlac77/sync-test-repository"
   ]);
-  console.log(p.stdout);
   t.is(p.code, 0);
+
+  console.log(p.stdout);
+
+  const m = p.stdout.match(/(\d+):/);
+  //console.log(m);
+
+  if (m) {
+    const provider = new GithubProvider(
+      GithubProvider.optionsFromEnvironment(process.env)
+    );
+    const repo = await provider.repository("arlac77/sync-test-repository");
+    await repo.deletePullRequest(m[1]);
+  }
 });
