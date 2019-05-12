e8a99ec8497574de55df02affab9128394b9380d
Markus Felten <Markus.Felten@gmx.de>
2019-05-12 20:10:11 +0200

    chore(travis): make use of builtins & cleanup more extensions

--- rollup.config.js
+++ rollup.config.js
@@ -6,48 +6,8 @@ import cleanup from "rollup-plugin-cleanup";
 import json from "rollup-plugin-json";
 import pkg from "./package.json";
 
-// require('module').builtinModules
 const external = [
-  "assert",
-  "async_hooks",
-  "buffer",
-  "child_process",
-  "cluster",
-  "console",
-  "constants",
-  "crypto",
-  "dgram",
-  "dns",
-  "domain",
-  "events",
-  "fs",
-  "http",
-  "http2",
-  "https",
-  "inspector",
-  "module",
-  "net",
-  "os",
-  "path",
-  "perf_hooks",
-  "process",
-  "punycode",
-  "querystring",
-  "readline",
-  "repl",
-  "stream",
-  "string_decoder",
-  "sys",
-  "timers",
-  "tls",
-  "trace_events",
-  "tty",
-  "url",
-  "util",
-  "v8",
-  "vm",
-  "zlib",
-
+  ...builtins,
   "node-fetch"
 ];
 
@@ -69,7 +29,8 @@ export default Object.keys(pkg.bin).map(name => {
         preferConst: true,
         compact: true
       }),
-      cleanup({
+      cleanup({    
+        extensions: ['js','mjs','jsx','tag'],
         exclude: [
           "node_modules/@octokit/plugin-throttling/lib/route-matcher.js"
         ]
