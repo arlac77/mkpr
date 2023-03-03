ae5980222984efb11f0d5b458c856bd099cf7de3
Markus Felten <Markus.Felten@gmx.de>
2023-03-03 12:32:36 +0100

    feat: --parallel execution of branches

--- src/mkpr-cli.mjs
+++ src/mkpr-cli.mjs
@@ -7,7 +7,10 @@ import { program } from "commander";
 import { applyPatch } from "fast-json-patch/index.mjs";
 import { StringContentEntry } from "content-entry";
 import { generateBranchName, asArray } from "repository-provider";
-import { initializeRepositoryProvider, initializeCommandLine } from "./setup-provider.mjs";
+import {
+  initializeRepositoryProvider,
+  initializeCommandLine
+} from "./setup-provider.mjs";
 import chalk from "chalk";
 
 const { version, description } = JSON.parse(
@@ -26,6 +29,7 @@ program
   .option("--dry", "do not create branch/pull request")
   .option("--trace", "log level trace")
   .option("--debug", "log level debug")
+  .option("--parallel", "execute branches in parallel")
   .option("-d, --define <...key=value>", "set option", values =>
     asArray(values).forEach(value => {
       const [k, v] = value.split(/=/);
@@ -64,107 +68,22 @@ program
         [exec, ...args] = exec.split(/\s+/);
       }
 
+      const patches = [];
       for await (const branch of provider.branches(branches)) {
-        try {
-          if (!branch.isWritable) {
-            console.log(chalk.gray(`${branch}: not writable - skip`));
-            continue;
-          }
-
-          const toBeCommited = [];
-          let numberOfEntries = 0;
-
-          for await (const entry of branch.entries(options.entries)) {
-            numberOfEntries++;
-
-            if (entry.isBlob) {
-              const originalString = await entry.string;
-              const originalLastChar =
-                originalString[originalString.length - 1];
-
-              let modified, newContent;
-              if (options.regex) {
-                const p = exec.split(/\//);
-                const regex = new RegExp(p[1], "g");
-                newContent = originalString.replace(regex, p[2]);
-              } else if (options.jsonpatch) {
-                console.log(`jsonpatch ${exec} ${branch} ${entry.name}`);
-
-                try {
-                  const patch = asArray(JSON.parse(exec));
-
-                  newContent = JSON.stringify(
-                    applyPatch(JSON.parse(originalString), patch).newDocument,
-                    undefined,
-                    2
-                  );
-                } catch (e) {
-                  if (e.name === "TEST_OPERATION_FAILED") {
-                    console.log("Skip: patch test not fullfilled", e.operation);
-                  } else {
-                    console.error(e);
-                  }
-                  continue;
-                }
-              } else {
-                const e = await execa(exec, args, {
-                  input: originalString
-                });
-
-                newContent = e.stdout;
-              }
-
-              const lastChar = newContent[newContent.length - 1];
-
-              // keep trailing newline
-              if (originalLastChar === "\n" && lastChar !== "\n") {
-                newContent += "\n";
-              }
-
-              modified = new StringContentEntry(entry.name, newContent);
-
-              const isEqual = await entry.equalsContent(modified);
-
-              if (!isEqual) {
-                toBeCommited.push(modified);
-              }
-            }
-          }
-
-          if (toBeCommited.length > 0) {
-            const pr = await branch.commitIntoPullRequest(
-              { message: options.message, entries: toBeCommited },
-              {
-                dry: options.dry,
-                pullRequestBranch: await generateBranchName(
-                  branch.repository,
-                  options.prbranch
-                ),
-                title:
-                  options.title === undefined ? options.message : options.title,
-                body: `Applied mkpr on ${options.entries}
-\`\`\`${options.jsonpatch ? "json" : "sh"}
-${exec} ${args}
-\`\`\`
-`
-              }
-            );
-
-            console.log(chalk.green(`${pr.identifier}: ${pr.title}`));
+        if (branch.isWritable) {
+          const p = patch(branch, exec, args, options);
+          if (options.parallel) {
+            patches.push(p);
           } else {
-            console.log(
-              chalk.gray(
-                `${branch.identifier}: ${
-                  numberOfEntries === 0
-                    ? "no matching entries"
-                    : "nothing changed"
-                }`
-              )
-            );
+            await p;
           }
-        } catch (err) {
-          console.error(err);
+        } else {
+          console.log(chalk.gray(`${branch}: not writable - skip`));
         }
+      }
+
+      if (patches.length) {
+        await Promise.all(patches);
       }
     } catch (err) {
       console.error(err);
@@ -172,3 +91,95 @@ ${exec} ${args}
     }
   })
   .parse(process.argv);
+
+async function patch(branch, exec, args, options) {
+  try {
+    const toBeCommited = [];
+    let numberOfEntries = 0;
+
+    for await (const entry of branch.entries(options.entries)) {
+      numberOfEntries++;
+
+      if (entry.isBlob) {
+        const originalString = await entry.string;
+        const originalLastChar = originalString[originalString.length - 1];
+
+        let modified, newContent;
+        if (options.regex) {
+          const p = exec.split(/\//);
+          const regex = new RegExp(p[1], "g");
+          newContent = originalString.replace(regex, p[2]);
+        } else if (options.jsonpatch) {
+          console.log(`jsonpatch ${exec} ${branch} ${entry.name}`);
+
+          try {
+            const patch = asArray(JSON.parse(exec));
+
+            newContent = JSON.stringify(
+              applyPatch(JSON.parse(originalString), patch).newDocument,
+              undefined,
+              2
+            );
+          } catch (e) {
+            if (e.name === "TEST_OPERATION_FAILED") {
+              console.log("Skip: patch test not fullfilled", e.operation);
+            } else {
+              console.error(e);
+            }
+            continue;
+          }
+        } else {
+          const e = await execa(exec, args, {
+            input: originalString
+          });
+
+          newContent = e.stdout;
+        }
+
+        const lastChar = newContent[newContent.length - 1];
+
+        // keep trailing newline
+        if (originalLastChar === "\n" && lastChar !== "\n") {
+          newContent += "\n";
+        }
+
+        modified = new StringContentEntry(entry.name, newContent);
+
+        if (!(await entry.equalsContent(modified))) {
+          toBeCommited.push(modified);
+        }
+      }
+    }
+
+    if (toBeCommited.length > 0) {
+      const pr = await branch.commitIntoPullRequest(
+        { message: options.message, entries: toBeCommited },
+        {
+          dry: options.dry,
+          pullRequestBranch: await generateBranchName(
+            branch.repository,
+            options.prbranch
+          ),
+          title: options.title === undefined ? options.message : options.title,
+          body: `Applied mkpr on ${options.entries}
+\`\`\`${options.jsonpatch ? "json" : "sh"}
+${exec} ${args}
+\`\`\`
+`
+        }
+      );
+
+      console.log(chalk.green(`${pr.identifier}: ${pr.title}`));
+    } else {
+      console.log(
+        chalk.gray(
+          `${branch.identifier}: ${
+            numberOfEntries === 0 ? "no matching entries" : "nothing changed"
+          }`
+        )
+      );
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
