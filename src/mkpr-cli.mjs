8c8982c988c186cf9e2442196015c3f5f2f86ce0
Markus Felten <Markus.Felten@gmx.de>
2024-03-28 00:57:16 +0100

    feat: set node process name

--- src/mkpr-cli.mjs
+++ src/mkpr-cli.mjs
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S node --no-warnings
+#!/usr/bin/env -S node --no-warnings --title mkpr
 import { execa } from "execa";
 import { program } from "commander";
 import chalk from "chalk";
